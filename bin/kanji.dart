9e57444323dc56c4cf2d33de5272e9ba43e8c428
Piotr Rogulski <piter.rogulski@gmail.com>
2025-03-23 17:44:25 +0100

    Create kanji parsing script

---
+++ bin/kanji.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file/file.dart';
+import 'package:file/local.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:kanji/model.dart';
+import 'package:mime/mime.dart';
+import 'package:yaml/yaml.dart';
+
+const fs = LocalFileSystem();
+
+void main(List<String> args) async {
+  final apiKey =
+      Platform.environment['GEMINI_API_KEY'] ??
+      error(r'No $GEMINI_API_KEY environment variable');
+
+  final imagePath = args.singleOrNull ?? error('Usage: kanji.dart <directory>');
+  final imageDir = fs.directory(imagePath);
+
+  if (!imageDir.existsSync()) {
+    error('Directory does not exist: $imagePath');
+  }
+
+  final model = createModel(apiKey: apiKey);
+
+  final images = imageDir
+      .listSync()
+      .whereType<File>()
+      .map((file) {
+        final bytes = file.readAsBytesSync();
+        final mime = lookupMimeType(
+          file.path,
+          headerBytes: Uint8List.sublistView(
+            bytes,
+            0,
+            defaultMagicNumbersMaxLength,
+          ),
+        );
+        return (mime, bytes);
+      })
+      .whereType<(String, Uint8List)>()
+      .where((e) => e.$1.startsWith('image/'))
+      .map((e) => Content.data(e.$1, e.$2));
+
+  final response = await model.generateContent(images);
+  final result = response.text;
+
+  if (result == null) {
+    error('Failed to generate content');
+  }
+
+  final yamlDir = imageDir.childDirectory('res')..createSync();
+  for (final yaml in loadYamlDocuments(result)) {
+    final contents = yaml.contents as YamlMap;
+    final id = contents['id'];
+    yamlDir.childFile('$id.yaml').writeAsStringSync(contents.span.text);
+  }
+}
+
+Never error(String message) {
+  stderr.writeln(message);
+  exit(1);
+}
---
+++ lib/model.dart
@@ -0,0 +1,72 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+GenerativeModel createModel({required String apiKey}) => GenerativeModel(
+  model: 'gemini-2.0-flash',
+  apiKey: apiKey,
+  generationConfig: GenerationConfig(
+    temperature: 0,
+    topK: 40,
+    topP: 0.95,
+    responseMimeType: 'text/plain',
+  ),
+  systemInstruction: Content.system(_system),
+);
+
+const _system = '''
+You'll be given entries from a Kanji textbook.
+Parse the entries to provide a YAML output with the data.
+Output only the YAML documents without the surrounding code fence.
+Output YAML documents separated with ---.
+This is an example of the schema:
+
+id: 10
+kanji: 九
+strokes: 2
+radical: 乙
+synonyms:
+  - 八
+antonyms:
+  - 一
+readings:
+  onyomi:
+    - キュウ
+    - ク
+  kunyomi:
+    - ここの
+    - ここの・つ
+examples:
+  - word: 土地
+    reading: トチ
+    meaning: ziemia; posiadłość ziemska; region
+    related:
+      - word: 土地の
+        reading: トチの
+        meaning: miejscowy, lokalny
+sentences:
+  - Watashi wa Pōrandojin desu.
+  - Kono fune no jin'in wa nannin desu ka.
+
+If an example uses an underscore, refer to the previous one, fill it in the underscore and nest it user the previous example, like this:
+```
+土地　トチ　...
+＿の　　　　...
+```
+becomes:
+```yaml
+examples:
+  - word: 土地
+    reading: トチ
+    meaning: ziemia; posiadłość ziemska; region
+    related:
+      - word: 土地の
+        reading: トチの
+        meaning: miejscowy, lokalny
+```
+
+Do not list it as a separate entry. Nest it inside the `related` list in the previous entry. Reuse the base word and the reading, but don't use additional spaces in the readings.
+
+If there's any additional handwriting visible in the image, ignore it and parse only the printed text.
+
+Some kun'youmi readings contain a dot – preserve the dot, since it represent a suffix. For example, "ちい・さい".
+
+Antonyms are represented with the ⇔ arrow, and synonyms with the ⇒ arrow.''';
